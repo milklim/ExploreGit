d1df17eb7aa4394c8591f9a8eb488328046fcc86
milklim <klimbox@gmail.com>
2016-05-24 22:38:17 +0300

    004: Call of the 'ShowStatus()' method

--- Task1_ExploreGit/Program.cs
+++ Task1_ExploreGit/Program.cs
@@ -12,6 +12,9 @@ namespace Task1_ExploreGit
             BSAcademy BSA2016 = new BSAcademy(2016, BSAcademy.Stages.Stage2_FirstLestures);
             BSA2016.About();
 
+            Console.WriteLine(new string('=', 40));
+            BSA2016.ShowStatus();
+
             Console.ReadKey();
         }
     }
